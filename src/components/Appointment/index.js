9257e182a6b7d230bde5ff8d34fccfd01823f992
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-01 10:00:52 -0700

    feat: implement useVisualMode hook for show and empty modes

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -4,22 +4,29 @@ import "components/Appointment/styles.scss";
 
 import Header from "components/Appointment/Header";
 
+import useVisualMode from "hooks/useVisualMode";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment" data-testid="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
-          onDelete={() => console.log("CONFIRM")}
-          onEdit={() => console.log("EDIT")}
         />
-        :
-        <Empty onAdd={() => console.log("CREATE")} />}
+      )}
     </article>
   );
 }

690688ded71e1e846655f1e2d2c714c77a03546d
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-01 09:43:08 -0700

    feat: add replace functionality to transition function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,12 +4,18 @@ const useVisualMode = (initial) => {
   const [ history, setHistory ] = useState([ initial ]);
   const mode = history[history.length - 1];
 
-  function transition(newMode){
-    setHistory(prev => [...prev, newMode]);
+  function transition(newMode, replace){
+    if (replace) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1), newMode]);
+    } else {
+      setHistory(prev => [...prev, newMode]);
+    }
   }
 
   function back() {
-    setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    if(history.length > 1) {
+      setHistory(prev => [...prev.slice(0, prev.length - 1)]);
+    };
   }
 
   return { mode, transition, back };

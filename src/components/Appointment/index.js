310a79f002e4ecd8b3223d4a3dd16c23417d1f1f
Kevin Levasseur <kevin_levasseur@telus.net>
2024-05-01 10:14:56 -0700

    feat: implement back button using useVisualMode hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -29,7 +29,10 @@ export default function Appointment(props) {
         />
       )}
       {mode === CREATE && (
-        <Form interviewers={props.interviewers} />
+        <Form
+          interviewers={props.interviewers}
+          onCancel={back}
+        />
       )}
     </article>
   );
